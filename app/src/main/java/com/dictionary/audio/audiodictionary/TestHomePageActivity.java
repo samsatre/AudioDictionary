dc460c6936a8b037f78e4e50b00ad6722b2ce965
Samuel <samuel.wang122@gmail.com>
2018-11-07 20:10:11 -0500

    Basic UI functionality of the initialization screen is done. The first time user uses the application, they choose the language they are learning and their preferred language. They only do this once. Once they make their choices and hit "submit" they can go on to the main/home screen. Right now it goes to a test page which shows their language choices.

--- app/src/main/java/com/dictionary/audio/audiodictionary/TestHomePageActivity.java
+++ app/src/main/java/com/dictionary/audio/audiodictionary/TestHomePageActivity.java
@@ -7,12 +7,15 @@ import android.widget.TextView;
 //TODO DO NOT RUN THIS CLASS WITHOUT INIT CLASS.
 public class TestHomePageActivity extends Activity{
 
+    private final String STATE_PREFERRED = "preferred";
+    private final String STATE_LEARN = "learn";
+
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
         Intent initIntent = getIntent();
-        String loadLearn = initIntent.getStringExtra("learn");
-        String loadPref = initIntent.getStringExtra("prefer");
+        String loadLearn = initIntent.getStringExtra(STATE_LEARN);
+        String loadPref = initIntent.getStringExtra(STATE_PREFERRED);
         setContentView(R.layout.activity_testhomepage);
         TextView learnView = findViewById(R.id.testHomeLearn);
         TextView prefView = findViewById(R.id.testHomePref);
