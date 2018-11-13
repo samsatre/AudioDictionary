3a1e8abca0568c5937347b8b695d3d24935276ff
Kira <vasquezk@umd.edu>
2018-11-12 20:43:57 -0500

    Home screen foundation added

--- app/src/main/java/com/dictionary/audio/audiodictionary/HomeScreenActivity.java
+++ app/src/main/java/com/dictionary/audio/audiodictionary/HomeScreenActivity.java
@@ -1,4 +1,32 @@
 package com.dictionary.audio.audiodictionary;
 
-public class HomeScreenActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeScreenActivity extends Activity {
+
+    private final String ENCOURAGE_MSG = "You have contributed 5 words!";
+    private final String CONTRIBUTE_TXT = "Contribute More Words";
+    private final String SEARCH_TXT = "Search New Words";
+    private final String FLASH_TXT = "Flash Cards";
+
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+
+        TextView encourageView = findViewById(R.id.encourageText);
+        Button contributeButton = findViewById(R.id.contributeButton);
+        Button searchButton = findViewById(R.id.searchButton);
+        Button flashButton = findViewById(R.id.flashButton);
+        encourageView.setText(ENCOURAGE_MSG);
+        contributeButton.setText(CONTRIBUTE_TXT);
+        searchButton.setText(SEARCH_TXT);
+        flashButton.setText(FLASH_TXT);
+
+    }
+
 }
