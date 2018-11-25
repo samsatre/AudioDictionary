1d175efcedc41f5de3a317ef71e1a300db599c2f
slsatre <slsatre@gmail.com>
2018-11-24 21:15:11 -0500

    ViewWord pulls from firebase now, play recording is not set up yet

--- app/src/main/java/com/dictionary/audio/audiodictionary/ViewWord.java
+++ app/src/main/java/com/dictionary/audio/audiodictionary/ViewWord.java
@@ -9,42 +9,62 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.ListView;
 import java.util.*;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ViewWord extends ListActivity {
     TextView wordText, translation1;
     ViewListAdapter mAdapter;
+    private DatabaseReference mBase;
+    private DatabaseReference mTable;
+    private DatabaseReference mWord;
+    private static String word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String tableName = "English";
+        word = "young";
+        mBase = FirebaseDatabase.getInstance().getReference();
+        DataSnapshot dataSnapshot;
+        mTable = mBase.child(tableName);
+        //mWord = mTable.child("apple");
+        //dataSnapshot = DataSnapshot(mWord,1).;
+        //word = DataSnapshotmWord.child("word")..toString();
+        //word = "test";
+        mTable.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        readData(dataSnapshot);
+                    }
 
-
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
         //this section will be from the data base using the uid from the intent
-        Map recordings = new HashMap();
+
+    }
+
+    private void readData(DataSnapshot dataSnapshot){
+        word = dataSnapshot.child(word).getValue(Word.class).getWord();
+
         List a = new ArrayList();
-        List<String> sentences = new ArrayList<String>(5);
-        List<String> definitions = new ArrayList<String>(5);
-        recordings.put("rec1",1);
-        sentences.add("I sat in the chair");
-        definitions.add("chair");
-        recordings.put("rec2",2);
-        sentences.add("He sat in the seat");
-        definitions.add("seat");
-        recordings.put("rec1",1);
-        sentences.add("I sat in the chair");
-        definitions.add("chair");
-        recordings.put("rec2",2);
-        sentences.add("He sat in the seat");
-        definitions.add("seat");
-        Word my_word = new Word("1","silla",sentences,recordings,definitions);
+        List<String> sentences = dataSnapshot.child(word).getValue(Word.class).getSentences();
+        List<String> definitions = dataSnapshot.child(word).getValue(Word.class).getDefinitions();
+        Map recordings = dataSnapshot.child(word).getValue(Word.class).getRecordings();
+        //Word my_word = new Word("1",word,sentences,recordings,definitions);
 
         //here on shouldn't have to change
         List<Item> items = new ArrayList<>(5);
         int i = 0;
         for (String s : sentences) {
             //items.add(new Item(s,(int)recordings.get("rec".concat(Integer.toString(i))),definitions.get(i)));
-            items.add(new Item(s,1,definitions.get(i)));
+            String def = definitions.get(i);
+            items.add(new Item(s,i,def));
             i++;
         }
         mAdapter = new ViewListAdapter(this, items);
@@ -58,7 +78,7 @@ public class ViewWord extends ListActivity {
         this.getListView().addHeaderView(theInflatedView);
 
         wordText = (TextView) this.getListView().findViewById(R.id.wordText);
-        wordText.setText(my_word.word);
+        wordText.setText(word);
         // Attach the adapter to a ListView
         this.getListView().setAdapter(mAdapter);
     }
