0b4ac123b005828e1936f86aa7de98cb252212e5
Samuel <samuel.wang122@gmail.com>
2018-12-01 22:47:28 -0500

    search bar final form probably completed.

--- app/src/main/java/com/dictionary/audio/audiodictionary/SearchActivity.java
+++ app/src/main/java/com/dictionary/audio/audiodictionary/SearchActivity.java
@@ -2,7 +2,9 @@ package com.dictionary.audio.audiodictionary;
 
 import android.app.Activity;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +26,21 @@ import com.google.firebase.database.ValueEventListener;
 public class SearchActivity extends Activity {
 
     private final String TAG = "AUDIODICTIONARY";
+    private final int ENGLISH = 1;
+    private final int SPANISH = 2;
+    private final int FRENCH = 3;
 
     private SearchView nativeSearch, learningSearch;
     private Button resultButton;
     private TextView resultText;
     private Button addWordButton;
+    private Spinner preferredSpinner;
+    private Spinner learningSpinner;
+    private final String STATE_PREFERRED = "preferred";
+    private final String STATE_LEARN = "learn";
+    private final String MyPrefs ="DictionaryPrefs";
+    SharedPreferences mSp;
+    SharedPreferences.Editor mEdit;
 
     private String learningLanguage = null;
     private String nativeLanguage = null;
@@ -44,7 +57,7 @@ public class SearchActivity extends Activity {
         setContentView(R.layout.search_word);
 
         nativeSearch = findViewById(R.id.nativeSearch);
-        learningSearch = findViewById(R.id.learningSearch);
+        //learningSearch = findViewById(R.id.learningSearch);
 
         resultText = findViewById(R.id.noResult);
         resultText.setVisibility(View.INVISIBLE);
@@ -55,12 +68,20 @@ public class SearchActivity extends Activity {
         addWordButton = findViewById(R.id.addWordButton);
         addWordButton.setVisibility(Button.INVISIBLE);
 
-        nativeSearch.clearFocus();
-        learningSearch.clearFocus();
+        preferredSpinner = findViewById(R.id.search_choose_preferred);
+        learningSpinner = findViewById(R.id.search_choose_learning);
 
-        // TODO: change to whatever language is selected
-        nativeLanguage = "French";
-        learningLanguage = "English";
+
+        nativeSearch.clearFocus();
+       // learningSearch.clearFocus();
+
+        mSp = getSharedPreferences(MyPrefs, Context.MODE_PRIVATE);
+        mEdit = mSp.edit();
+        nativeLanguage = mSp.getString(STATE_PREFERRED,"not found prefer");
+        learningLanguage = mSp.getString(STATE_LEARN,"not found learn");
+        learningSpinner.setSelection(getIndex(learningSpinner,learningLanguage));
+        preferredSpinner.setSelection(getIndex(preferredSpinner,nativeLanguage));
+
 
         firebaseDatabase = FirebaseDatabase.getInstance();
        // nativeDatabaseReference = firebaseDatabase.getReference(nativeLanguage);
@@ -69,7 +90,7 @@ public class SearchActivity extends Activity {
         Intent searchIntent = getIntent();
         if (searchIntent != null && searchIntent.hasExtra(SearchManager.QUERY)) {
             String search = searchIntent.getStringExtra(SearchManager.QUERY);
-            learningSearch.setQuery(search, true);
+            nativeSearch.setQuery(search, true);
             language = learningLanguage + "-" + nativeLanguage;
             querySearched(search, language);
         }
@@ -78,7 +99,8 @@ public class SearchActivity extends Activity {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 nativeSearch.clearFocus();
-                language = nativeLanguage + "-" + learningLanguage;
+                language = preferredSpinner.getSelectedItem().toString() + "-"
+                        + learningSpinner.getSelectedItem().toString();
                 querySearched(s, language);
                 return true;
             }
@@ -89,7 +111,7 @@ public class SearchActivity extends Activity {
             }
         });
 
-        learningSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        /*learningSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 learningSearch.clearFocus();
@@ -102,7 +124,19 @@ public class SearchActivity extends Activity {
             public boolean onQueryTextChange(String s) {
                 return false;
             }
-        });
+        });*/
+    }
+
+    private int getIndex(Spinner spinner, String target){
+
+        int index = 0;
+
+        for (int i=0;i<spinner.getCount();i++){
+            if (spinner.getItemAtPosition(i).equals(target)){
+                index = i;
+            }
+        }
+        return index;
     }
 
     private void querySearched (final String queryString, final String language) {
