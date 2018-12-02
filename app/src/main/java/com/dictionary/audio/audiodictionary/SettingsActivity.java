e1a186b5cb28e9529a607b058e93c087874b3645
Samuel <samuel.wang122@gmail.com>
2018-12-02 02:37:50 -0500

    settings page is partially done, just need to implement change username and password.

--- app/src/main/java/com/dictionary/audio/audiodictionary/SettingsActivity.java
+++ app/src/main/java/com/dictionary/audio/audiodictionary/SettingsActivity.java
@@ -3,12 +3,16 @@ package com.dictionary.audio.audiodictionary;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,6 +21,14 @@ public class SettingsActivity extends Activity {
     Button mSaveBtn;
     Button mSavePref;
     Button mSaveLearn;
+    Spinner preferSpin;
+    Spinner learnSpin;
+    SharedPreferences mSp;
+    SharedPreferences.Editor mEdit;
+    private final String MyPrefs ="DictionaryPrefs";
+    private final String STATE_PREFERRED = "preferred";
+    private final String STATE_LEARN = "learn";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -26,10 +38,53 @@ public class SettingsActivity extends Activity {
 
         mSaveBtn = findViewById(R.id.settings_save_button);
         mSavePref = findViewById(R.id.settings_prefer_btn);
+        mSaveLearn = findViewById(R.id.settings_learn_btn);
+        preferSpin = findViewById(R.id.nativeSpinner);
+        learnSpin = findViewById(R.id.learnSpinner);
+        mSp = getSharedPreferences(MyPrefs, Context.MODE_PRIVATE);
+        mEdit = mSp.edit();
+        String nativeLanguage = mSp.getString(STATE_PREFERRED,"not found prefer");
+        String learningLanguage = mSp.getString(STATE_LEARN,"not found learn");
+        learnSpin.setSelection(getIndex(learnSpin,learningLanguage));
+        preferSpin.setSelection(getIndex(preferSpin,nativeLanguage));
+
+        mSavePref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mEdit.putString(STATE_PREFERRED,preferSpin.getSelectedItem().toString());
+                mEdit.commit();
+                Toast.makeText(getApplicationContext(),"Prefered language saved!",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        mSaveLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mEdit.putString(STATE_LEARN,learnSpin.getSelectedItem().toString());
+                mEdit.commit();
+                Toast.makeText(getApplicationContext(),"Language to learn saved!",Toast.LENGTH_LONG).show();
+
+
+            }
+        });
 
 
 
 
+    }
+
+    private int getIndex(Spinner spinner, String target){
+
+        int index = 0;
+
+        for (int i=0;i<spinner.getCount();i++){
+            if (spinner.getItemAtPosition(i).equals(target)){
+                index = i;
+            }
+        }
+        return index;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
